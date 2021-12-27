422133fc3996a24bcd323da7a730d96c39477639
Wataru NAKAI <watnk0806@gmail.com>
2021-12-27 13:07:35 +0900

    PAST 201912 H; AC

--- chap06/past201912_h.rb
+++ chap06/past201912_h.rb
@@ -2,7 +2,15 @@ n = gets.chomp.to_i
 c = gets.chomp.split(" ").map(&:to_i)
 q = gets.chomp.to_i
 
+min_odd = (1..n).filter_map { |i| c[i - 1] if i.odd? }.min
+n_odd = n / 2
+n_odd += 1 if n.odd?
+
+min_all = c.min
+
 ans = 0
+to_sum_odd = 0
+to_sum_all = 0
 
 q.times do
   s = gets.chomp.split(" ").map(&:to_i)
@@ -10,30 +18,37 @@ q.times do
   if s[0] == 1
     x, a = s[1] - 1, s[2]
 
-    if c[x] >= a
-      ans += a
-      c[x] -= a
+    if (x + 1).odd?
+      if c[x] - to_sum_odd - to_sum_all >= a
+        ans += a
+        c[x] -= a
+        min_odd = [min_odd, c[x]].min
+        min_all = [min_odd, min_all].min
+      end
+    else
+      if c[x] - to_sum_all >= a
+        ans += a
+        c[x] -= a
+        min_all = [min_all, c[x]].min
+      end
     end
   elsif s[0] == 2
     a = s[1]
 
-    if (0...n).step(2).map { |i| c[i] }.min >= a
-      (0...n).step(2) do |i|
-        ans += a
-        c[i] -= a
-      end
+    if min_odd >= a
+      to_sum_odd += a
+      min_odd -= a
+      min_all = [min_odd, min_all].min
     end
   else
-
     a = s[1]
 
-    if c.min >= a
-      n.times do |i|
-        ans += a
-        c[i] -= a
-      end
+    if min_all >= a
+      to_sum_all += a
+      min_odd -= a
+      min_all -= a
     end
   end
 end
 
-puts(ans)
+puts(ans + to_sum_odd * n_odd + to_sum_all * n)
