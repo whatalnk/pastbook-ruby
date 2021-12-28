f466f05dc06dd4316c204f83697131d271d1cf03
Wataru NAKAI <watnk0806@gmail.com>
2021-12-28 17:01:32 +0900

    PAST 202005 G; AC

--- chap06/past202005_g.rb
+++ chap06/past202005_g.rb
@@ -1,22 +1,27 @@
+offset = 250
+
 n, x, y = gets.chomp.split(" ").map(&:to_i)
-x += 200
-y += 200
+x += offset
+y += offset
+
+sx = offset
+sy = offset
 
 m = Array.new(500) { Array.new(500, ".") }
 d = Array.new(500) { Array.new(500, -1) }
 
 n.times do
   xi, yi = gets.chomp.split(" ").map(&:to_i)
-  xi += 200
-  yi += 200
-  m[yi][xi] = "#"
+  xi += offset
+  yi += offset
+  m[xi][yi] = "#"
 end
 
 dxdy = [[1, 1], [0, 1], [-1, 1], [1, 0], [-1, 0], [0, -1]]
 
 que = []
-d[200][200] = 0
-que << [200, 200]
+d[sx][sy] = 0
+que << [sx, sy]
 
 while que.size > 0
   cx, cy = que.shift
@@ -24,12 +29,11 @@ while que.size > 0
   dxdy.each do |dx, dy|
     nx, ny = cx + dx, cy + dy
 
-    if nx >= 0 && nx < 500 && ny >= 0 && ny < 500 && m[ny][nx] != "#" && d[ny][nx] == -1
-
-      d[ny][nx] = d[cy][cx] + 1
+    if nx >= 0 && nx < 500 && ny >= 0 && ny < 500 && m[nx][ny] != "#" && d[nx][ny] == -1
+      d[nx][ny] = d[cx][cy] + 1
       que << [nx, ny]
     end
   end
 end
 
-puts(d[y][x])
+puts(d[x][y])
