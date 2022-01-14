b2aba113f3a863cf9f561d8292e477f1ec237cf7
Wataru NAKAI <watnk0806@gmail.com>
2022-01-14 11:55:05 +0900

    Update PAST202005 C

--- chap06/past202005_c.rb
+++ chap06/past202005_c.rb
@@ -1,7 +1,16 @@
 # 6.6 数学的問題
 A, R, N = gets.chomp.split(" ").map(&:to_i)
 
-num = A * (R ** (N - 1))
+if R == 1
+  num = A * (R ** (N - 1))
+else
+  num = A
+
+  (N - 1).times do
+    num *= R
+    break if num > 10 ** 9
+  end
+end
 
 if num > 10 ** 9
   puts("large")
