8d65a764f4918d834e654dbc3929e18cd06a74fb
Wataru NAKAI <watnk0806@gmail.com>
2022-01-24 23:47:15 +0900

    ARC079 A; AC

--- chap06/arc079_a.rb
+++ chap06/arc079_a.rb
@@ -10,3 +10,25 @@ M.times do
   g[a] << b
   g[b] << a
 end
+
+d = Array.new(N, -1)
+q = [0]
+
+d[0] = 0
+
+while !q.empty?
+  i = q.shift
+
+  g[i].each do |j|
+    if d[j] == -1
+      d[j] = d[i] + 1
+      q << j
+    end
+  end
+end
+
+if d[N - 1] == 2
+  puts("POSSIBLE")
+else
+  puts("IMPOSSIBLE")
+end
