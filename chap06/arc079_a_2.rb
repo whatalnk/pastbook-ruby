599582c9c99d73ea9137628bc36568d3590cd923
Wataru NAKAI <watnk0806@gmail.com>
2022-01-26 00:39:16 +0900

    binary heap

--- chap06/arc079_a_2.rb
+++ chap06/arc079_a_2.rb
@@ -18,23 +18,67 @@ class BinaryHeap
   end
 
   def add(x)
-    if n + 1 > @a.size
+    if @n + 1 > @a.size
       resize
     end
 
-    @n += 1
     @a[@n] = x
+    @n += 1
+
     bubble_up(@n - 1)
     return true
   end
 
-  def bubble_up
+  def bubble_up(i)
+    p_ = parent(i)
+    while i > 0 && @a[i] < @a[p_]
+      @a[i], @a[p_] = @a[p_], @a[i]
+      i = p_
+      p_ = parent(i)
+    end
   end
 
   def remove
+    x = @a[0]
+    @a[0] = @a[@n - 1]
+    @n -= 1
+    trickle_down(0)
+
+    if (3 * n < @a.size)
+      resize
+    end
+
+    return x
   end
 
-  def trickle_down
+  def trickle_down(i)
+    while true
+      j = -1
+      r = right(i)
+
+      if r < @n && @a[r] < @a[i]
+        l = left(i)
+
+        if @a[l] < @a[r]
+          j = l
+        else
+          j = r
+        end
+      else
+        l = left(i)
+
+        if l < n && @a[l] < @a[i]
+          j = l
+        end
+      end
+
+      if j >= 0
+        @a[i], @a[j] = @a[j], @a[i]
+      end
+
+      i = j
+      break if i < 0
+    end
   end
 
   def resize
