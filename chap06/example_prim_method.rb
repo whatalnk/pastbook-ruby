c0a4aa45b4f8e6e1ccc112fc0cea4116326b268d
Wataru NAKAI <watnk0806@gmail.com>
2022-01-29 21:15:49 +0900

    prim s method; example

---
+++ chap06/example_prim_method.rb
@@ -0,0 +1,75 @@
+Node = Struct.new(:x, :left, :right, :parent)
+
+class MeldableHeap
+  attr_accessor :r, :n
+  def initialize
+    @r = nil
+    @n = 0
+  end
+
+  def compare(x, y)
+    x.c - y.c
+  end
+
+  def merge(h1, h2)
+    return h2 if h1.nil?
+    return h1 if h2.nil?
+    return merge(h2, h1) if compare(h1.x, h2.x) > 0
+
+    if Random.rand(100).even?
+      h1.left = merge(h1.left, h2)
+      h1.left.parent = h1 if !h1.left.nil?
+    else
+      h1.right = merge(h1.right, h2)
+      h1.right.parent = h1 unless h1.right.nil?
+    end
+
+    return h1
+  end
+
+  def add(x)
+    u = Node.new(x, nil, nil, nil)
+    @r = merge(u, @r)
+    @r.parent = nil
+    @n += 1
+    return true
+  end
+
+  def remove
+    x = @r.x
+    @r = merge(r.left, r.right)
+    @r.parent = nil unless @r.nil?
+    @n -= 1
+    return x
+  end
+end
+
+marked = Array.new(N, false)
+
+marked_count = 0
+
+marked[0] = 0
+marked_count += 1
+
+q = MeldableHeap.new
+Tuple = Struct.new(:c, :j)
+
+g[0].each do |j, c|
+  q.add(Tuple.new(c, j))
+end
+
+sum = 0
+
+while marked_count < N
+  ci = q.remove
+  next if marked(ci.i)
+  marked[ci.i] = true
+  marked_count += 1
+
+  g[ci.i].each do |j, c|
+    next if marked[j]
+    q.add(Tuple.new(c, j))
+  end
+end
+
+puts(sum)
